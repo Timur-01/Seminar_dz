03bec9b0116daed4b2e738cfed3bcfa40d1c8321
Timur <cpdbox@mail.ru>
2023-06-21 23:49:07 +0300

    Add dz 2

--- src/dz2.java
+++ src/dz2.java
@@ -1,6 +1,3 @@
-public class dz2 {
-
-    import java.io.*;
     public class dz2 {
 
 /*Дана строка sql-запроса "select * from students where ".
@@ -9,35 +6,37 @@ public class dz2 {
 Если значение null, то параметр не должен попадать в запрос.
 Параметры для фильтрации: {"name":"Ivanov", "country":"Russia", "city":"Moscow", "age":"null"} */
 
-//    public static void main(String[] args) {
-//
-//        String s = ("{\"name\":\"Ivanov\"," +
-//                " \"country\":\"Russia\", \"city\":\"Moscow\", \"age\":\"null\"}");
-//
-//        String line = s.replace("{", "").replace("}", "").replace("\"", "");
-//        System.out.println(line);
-//
-//        StringBuilder result = new StringBuilder("select * from students where ");
-//
-//        String [] arrayData = line3.split(", ");
-//        for (int i =0; i < arrayData.length; i++) {
-//            String[] arrData = arrayData[i].split(":");
-//            if (arrData[1].equals("null") == false) {
-//                if (i != 0) {
-//                    result.append(", ");
-//                    result.append(arrData[0]);
-//                    result.append(" = ");
-//                    result.append(arrData[1]);
-//                } else {
-//                    result.append(arrData[0]);
-//                    result.append(" = ");
-//                    result.append(arrData[1]);
-//                }
-//
-//            }
-//        }
-//        System.out.println(result);
-//    }
+    public static void main(String[] args) {
+
+        String s = ("{\"name\":\"Ivanov\"," +
+                " \"country\":\"Russia\", \"city\":\"Moscow\", \"age\":\"null\"}");
+
+        String line = s.replace("{", "").replace("}", "").replace("\"", "");
+        System.out.println(line);
+
+        StringBuilder result = new StringBuilder("select * from students where ");
+
+        String [] arrayData = line3.split(", ");
+        for (int i =0; i < arrayData.length; i++) {
+            String[] arrData = arrayData[i].split(":");
+            if (arrData[1].equals("null") == false) {
+                if (i != 0) {
+                    result.append(", ");
+                    result.append(arrData[0]);
+                    result.append(" = ");
+                    result.append(arrData[1]);
+                } else {
+                    result.append(arrData[0]);
+                    result.append(" = ");
+                    result.append(arrData[1]);
+                }
+
+            }
+        }
+        System.out.println(result);
+    }
+
+
 /* Дана json строка (можно сохранить в файл и читать из файла)
 [{"фамилия":"Иванов","оценка":"5","предмет":"Математика"},{"фамилия":"Петрова","оценка":"4","предмет":"Информатика"},{"фамилия":"Краснов","оценка":"5","предмет":"Физика"}]
 Написать метод(ы), который распарсит json и, используя StringBuilder, создаст строки вида: Студент [фамилия] получил [оценка] по предмету [предмет].
@@ -45,18 +44,98 @@ public class dz2 {
 Студент Иванов получил 5 по предмету Математика.
 Студент Петрова получил 4 по предмету Информатика.
 Студент Краснов получил 5 по предмету Физика. */
-//    public static void main(String[] args) {
-//        StringBuilder builder = new StringBuilder({"фамилия":"Иванов","оценка":"5","предмет":"Математика"},{"фамилия":"Петрова","оценка":"4","предмет":"Информатика"},{"фамилия":"Краснов","оценка":"5","предмет":"Физика"});
-//        System.out.println(builder);
-//    }
+
+        //Споткнулся на простом, не смог строку перевести в массив
+
+        public static void main(String[] args) {
+            String[] arrayData = [{"фамилия":"Иванов","оценка":"5","предмет":"Математика"},{"фамилия":"Петрова","оценка":"4","предмет":"Информатика"},{"фамилия":"Краснов","оценка":"5","предмет":"Физика"}];
+
+
+         {
+            String line1 = line.replace("{", "");
+            String line2 = line1.replace("}", "");
+            String line3 = line2.replaceAll("\"", "");
+            String line4 = line3.replace("[", "");
+            String line5 = line4.replace("]", "");
+            StringBuilder result = new StringBuilder("");
+            String[] arrayData = line5.split(",");
+            String[] listName = {"Студент ", " получил ", " по предмету "};
+            for (int i = 0; i < arrayData.length; i++) {
+                String[] arrData = arrayData[i].split(":");
+                result.append(listName[i]);
+                result.append(arrData[1]);
+            }
+            return result;
+        }
+    }
+
+        /*Сравнить время выполнения замены символа "а" на "А" любой строки содержащей
+        1000 символов средствами String и StringBuilder.*/
 
         public static void main(String[] args) {
 
+            String s = "I love thee, city of peter's making" +
+                    "i love thy harmonies austere" +
+                    "And Neva's Sovran waters breaking" +
+                    "Along the banks of granite sheer" +
+                    "Thy traceried iron gates, thy sparkling" +
+                    "Yet Moouless, mediative gloom" +
+                    "And thy transparent twilight darkling" +
+                    "And when I write within my room" +
+                    "or Lampless, read - then, sunk, in slumber" +
+                    "The empty thoroughfares, past number" +
+                    "Are piled, stand clear upon the night" +
+                    "The admiralty spire is bright" +
+                    "Nor may the darkness mount, to smother" +
+                    "The golden cloudland of the light" +
+                    "For soon one dawn succeeds another" +
+                    "With barely half-an-hour of night" +
+                    "I love thy ruthless winter, lowering" +
+                    "With bitter frost and windless air" +
+                    "The sledges along Neva scouring" +
+                    "Girls' cheeks - no roses so bright and fair!" +
+                    "The flash and noise of balls, the chatter" +
+                    "The bachelor's house of feasting, too" +
+                    "The cups that foam and hiss and spatter" +
+                    "The punch that in the bowl burns blue" +
+                    "I love the warlike animation" +
+                    "On playing-fields of Mars to see" +
+                    "The troops of foot and horse in station" +
+                    "And their superb monotony" +
+                    "Their ordered, undulating muster" +
+                    "Flags tattered on the glorious day" +
+                    "Those brazen helmets in their luster" +
+                    "Shot through and riddled in the fray" +
+                    "I love thee, city of soldiers, blowing" +
+                    "Smoke from thy forts, thy booming gun" +
+                    "-- Northern empress is bestowing" +
+                    "Upon the royal house a son" +
+                    "Or when, another battle won," +
+                    "Proud Russia holds her celebration" +
+                    "Or when the Neva breaking free" +
+                    "Her dark-blue ice bears out to sea" +
+                    "And scents the spring, in exultation" +
+                    "Now, city of Peter, stand thou fast" +
+                    "Foursquare like Russia, vaunt thy splendor!" +
+                    "The very element shall surrender" +
+                    "And make her peace with thee at last" +
+                    "Their ancient bondage and their rancors" +
+                    "The finnish waves shall buy deep" +
+                    "Nor vex with idle spite that cankers" +
+                    "Our Peter's everlasting sleep!";
 
 
-            String[] arrayData = new arrayData([{"фамилия":"Иванов","оценка":"5","предмет":"Математика"},{"фамилия":"Петрова","оценка":"4","предмет":"Информатика"},{"фамилия":"Краснов","оценка":"5","предмет":"Физика"}]);
+            long start = System.nanoTime();
+            String text = s.replace("a", "A");
+            System.out.println(System.nanoTime() - start);
 
+            start = System.nanoTime();
+            StringBuilder text2 = new StringBuilder(text);
+                int i = text2.indexOf("a");
+            while(i > 0){
+                text2.replace(i,i+1,"a");
+                i = text2.indexOf("A");
+            }
+            System.out.println(System.nanoTime() - start);
         }
-
-
     }
